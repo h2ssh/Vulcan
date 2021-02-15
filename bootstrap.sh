da0a2bb0055e974ec97aec01ace2bde24bfa73ee
Collin Johnson <collinej@umich.edu>
2021-02-14 23:11:08 -0500

    Finish install-with-meson
    Meson-based install.sh + bootstrap.sh now work with a fresh install of 20.04. There is not yet full parity with SCons-based build, mostly related to low-level Quantum6000 wheelchair support. High level functionality is there though.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-meson setup --wipe --prefix=$PWD --werror --warnlevel=3 --buildtype=debugoptimized build
+rm -rf build/* bin/*
+meson setup --prefix=$PWD --werror --warnlevel=3 --buildtype=debugoptimized --pkg-config-path=external/pkgconfig,external/lib/pkgconfig $@ build
--- install.sh
+++ install.sh
@@ -5,26 +5,28 @@ echo "Installing Vulcan from"$DIR
 echo "To install drivers for using the Vulcan wheelchair, set the DRIVERS environment variable:  DRIVERS=1 \
  $DIR/install.sh"
 
-sudo apt-get install -y wget git scons doxygen texlive-latex-extra libboost-all-dev libarmadillo-dev libwxgtk3.0-gtk3-dev libwxgtk-media3.0-gtk3-dev libf2c2-dev openjdk-8-jdk libusb-dev libusb-1.0-0-dev libpopt-dev libsdl1.2-dev libsdl-net1.2-dev libopencv-dev cmake autoconf;
+sudo apt install -y build-essential meson ninja-build ccache wget liblcm-dev googletest doxygen texlive-latex-extra libboost-all-dev libarmadillo-dev libwxgtk3.0-gtk3-dev libwxgtk-media3.0-gtk3-dev libusb-dev libusb-1.0-0-dev libpopt-dev libsdl1.2-dev libsdl-net1.2-dev libopencv-dev libf2c2-dev cmake;
 
 EXTERNAL_DIR=$DIR/external
+LIB_DIR=$EXTERNAL_DIR/lib
+INCLUDE_DIR=$EXTERNAL_DIR/include
+PKG_CONFIG_DIR=$EXTERNAL_DIR/pkgconfig
+
 cd $EXTERNAL_DIR
+
+mkdir include
+mkdir lib
+mkdir pkgconfig
+
+# FIX for libarmadillo-dev not including a pkg-config (fixed upstream)
+cp armadillo.pc $PKG_CONFIG_DIR
 
 # Install cereal for message passing serialization
 wget -nc https://github.com/USCiLab/cereal/archive/v1.2.1.tar.gz -O cereal-1.2.1.tar.gz
 tar -xzf cereal-1.2.1.tar.gz
 rsync -a cereal-1.2.1/ cereal
 rm -rf cereal-1.2.1/
-
-# Install LCM
-wget -nc https://github.com/lcm-proj/lcm/archive/v1.3.1.tar.gz -O lcm-1.3.1.tar.gz
-tar -xzf lcm-1.3.1.tar.gz
-cd lcm-1.3.1
-./bootstrap.sh
-./configure
-make -j5
-sudo make install
-cd $EXTERNAL_DIR
+rsync -a cereal/include/ $INCLUDE_DIR
 
 # Install NLopt for running MPEPC
 wget -nc https://github.com/stevengj/nlopt/archive/v2.5.0.tar.gz -O nlopt-2.5.0.tar.gz
@@ -32,9 +34,9 @@ tar -xzf nlopt-2.5.0.tar.gz
 cd nlopt-2.5.0
 mkdir build
 cd build
-cmake ../ -DBUILD_SHARED_LIB=0
+cmake ../ -DBUILD_SHARED_LIBS=0 -DCMAKE_INSTALL_PREFIX=$EXTERNAL_DIR
 make -j5
-sudo make install
+make install
 cd $EXTERNAL_DIR
 
 # Install levmar for use with topological SLAM
@@ -42,28 +44,34 @@ cd $EXTERNAL_DIR
 tar -xzf levmar-2.6.tgz
 cd levmar-2.6
 make
-sudo cp liblevmar.a /usr/local/lib/
+cp liblevmar.a $LIB_DIR
 chmod a+r levmar.h
-sudo cp levmar.h /usr/local/include/
-sudo cp levmar.pc /usr/local/lib/pkgconfig/
+cp levmar.h $INCLUDE_DIR
 cd $EXTERNAL_DIR/
-
+# Prepend the prefix with the absolute path to the install directory
+echo "prefix=$PKG_CONFIG_DIR" > $PKG_CONFIG_DIR/levmar.pc
+cat levmar.pc >> $PKG_CONFIG_DIR/levmar.pc
 
 # Install libraries for place labeling
 wget -nc https://github.com/cjlin1/liblinear/archive/v220.tar.gz -O liblinear.tar.gz
 tar -xzf liblinear.tar.gz
 rsync -a liblinear-220/ liblinear
 rm -rf liblinear-220/
+mkdir $INCLUDE_DIR/liblinear
+cp liblinear/linear.h $INCLUDE_DIR/liblinear
 
 wget -nc https://github.com/cjlin1/libsvm/archive/v323.tar.gz -O libsvm.tar.gz
 tar -xzf libsvm.tar.gz
 rsync -a libsvm-323/ libsvm
 rm -rf libsvm-323/
+mkdir $INCLUDE_DIR/libsvm
+cp libsvm/svm.h $INCLUDE_DIR/libsvm
 
 # Install gnuplot-iostream used internally for debugging
 git clone https://github.com/dstahlke/gnuplot-iostream.git gpis_tmp
 rsync -a gpis_tmp/ gnuplot-iostream
 rm -rf gpis_tmp
+cp gnuplot-iostream/gnuplot-iostream.h $INCLUDE_DIR
 
 ######   Drivers for Vulcan wheelchair ######
 
@@ -76,8 +84,8 @@ if [ "$DRIVERS" = "1" ]; then
     cd peak-linux-driver-8.5.1
     make NET=NO_NETDEV_SUPPORT
     sudo make install
-    sudo cp driver/pcan.h /usr/local/include/
-    sudo cp lib/libpcan.h /usr/local/include/
+    cp driver/pcan.h $INCLUDE_DIR
+    cp lib/libpcan.h $INCLUDE_DIR
     cd $EXTERNAL_DIR
 
     # Install libphidget for talking with the encoders
@@ -85,25 +93,26 @@ if [ "$DRIVERS" = "1" ]; then
     wget -nc https://www.phidgets.com/downloads/phidget21/libraries/linux/libphidget/libphidget_2.1.8.20151217.tar.gz
     tar -xzf libphidget_2.1.8.20151217.tar.gz
     cd libphidget-2.1.8.20151217
-    ./configure
+    ./configure --prefix=$EXTERNAL_DIR
     make -j5
-    sudo make install
+    make install
     cd $EXTERNAL_DIR
 
     # Install URG library for talking to Hokuyos
     tar -xzf urg-0.8.18.tar.gz
     cd urg-0.8.18
-    ./configure LIBS=-lm
+    ./configure LIBS=-lm --prefix=$EXTERNAL_DIR
     make
-    sudo make install
-    sudo cp src/cpp/urg/ScipHandler.h /usr/local/include/urg/
+    make install
+    cp src/cpp/urg/ScipHandler.h $INCLUDE_DIR/urg
     cd $EXTERNAL_DIR
-    sudo cp liburg.pc /usr/local/lib/pkgconfig/
+    # Prepend the prefix with the absolute path to the install directory
+    echo "prefix=$PKG_CONFIG_DIR" > $PKG_CONFIG_DIR/liburg.pc
+    cat liburg.pc >> $PKG_CONFIG_DIR/liburg.pc
 fi
 
 # Go back to root Vulcan directory and run the bootstrap for generating LCM messages
 cd $DIR
 ./bootstrap.sh
 
-echo "All dependencies are installed. You can build the software with 'scons -jN' where N is the number of parallel commands to execute"
-
+echo "All dependencies are installed. You can build the software with 'ninja -C build install' Binaries will be installed in the bin directory."
